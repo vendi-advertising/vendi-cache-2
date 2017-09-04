2d6dbacc94439fceb6f834683e016e6324180c59
Chris Haas <chris@vendiadvertising.com>
2017-09-04 11:30:15 -0500

    Bootstrap needs to include plugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,3 +7,11 @@ require_once $_tests_dir . '/includes/functions.php';
 
 // Start up the WP testing environment.
 require_once $_tests_dir . '/includes/bootstrap.php';
+
+// tests_add_filter(
+//                     'muplugins_loaded',
+//                     function()
+//                     {
+                        require dirname( dirname( __FILE__ ) ) . '/vendi-cache.php';
+                    // }
+                // );
