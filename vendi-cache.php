ce5fe9f1bff0e0b29920a0b1fd2ea119cc8d45e5
Chris Haas <chris@vendiadvertising.com>
2017-09-11 16:04:54 -0500

    Remove constants, only use for hard-overrides instead

--- vendi-cache.php
+++ vendi-cache.php
@@ -18,55 +18,8 @@ define( 'VENDI_CACHE_URL', plugin_dir_url( __FILE__ ) );
 define( 'VENDI_CACHE_VERSION', '2.0.0' );
 
 require_once VENDI_CACHE_DIR . '/includes/autoload.php';
-require_once VENDI_CACHE_DIR . '/includes/constants.php';
-
-// if( VENDI_CACHE_LOG_LEVEL )
-// {
-//     //This is used to trace a specific request through the pipeline
-//     define( 'VENDI_CACHE_REQUEST_ID', \Ramsey\Uuid\Uuid::uuid4() );
-// }
-//
+// require_once VENDI_CACHE_DIR . '/includes/constants.php';
 
 \Vendi\Cache\Logging::get_instance()->debug( 'Plugin loading' );
 
 require_once VENDI_CACHE_DIR . '/includes/hooks.php';
-
-// \Vendi\Cache\CacheMaster::get_instance()->setup_caching();
-
-
-
-
-// \Vendi\Cache\Logging::get_instance()->warning( 'Hello World', ['cheese' => true] );
-
-
-// $url = 'https://www.vendiadvertising.com/work/';
-
-// $html = wp_remote_get( $url );
-
-// $file = \Vendi\Cache\CacheKeyGenerator::local_cache_filename_from_url( $url );
-
-// $adapter = new \League\Flysystem\Adapter\Local(
-//                                                 VENDI_CACHE_FOLDER_ABS,
-//                                                 LOCK_EX,
-//                                                 \League\Flysystem\Adapter\Local::DISALLOW_LINKS,
-//                                                 [
-//                                                     'file' => [
-//                                                         'public'  => 0664,
-//                                                         'private' => 0664,
-//                                                     ],
-//                                                     'dir' => [
-//                                                         'public'  => 0755,
-//                                                         'private' => 0755,
-//                                                     ]
-//                                                 ]
-//                                             );
-// $filesystem = new \League\Flysystem\Filesystem( $adapter );
-
-// dump( $filesystem->has( $file ) );
-
-// $filesystem->write( $file, $html[ 'body' ] );
-
-// // $cache->set( fix_tag( $url ), $html[ 'body' ] );
-
-
-// dump(  'https://www.vendiadvertising.com/' ) );
