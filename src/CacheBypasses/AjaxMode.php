71da6b9502080351be7753c5237e7ee2dd97c168
cjhaas <chris@vendiadvertising.com>
2017-10-03 15:28:16 -0500

    Break logic into multiple IF statements for testing

--- src/CacheBypasses/AjaxMode.php
+++ src/CacheBypasses/AjaxMode.php
@@ -6,11 +6,23 @@ final class AjaxMode extends AbstractCacheBypass
 {
     public function is_cacheable( )
     {
-        if( ( function_exists( 'wp_doing_ajax' ) && wp_doing_ajax() ) || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) )
+        if( function_exists( 'wp_doing_ajax' ) && wp_doing_ajax() )
         {
             $this->log_request_as_not_cacheable(
                                                     [
                                                         'reason' => 'Request is AJAX',
+                                                        'extra'  => 'Function wp_doing_ajax returned true',
+                                                    ]
+                                            );
+            return false;
+        }
+
+        if( defined( 'DOING_AJAX' ) && DOING_AJAX )
+        {
+            $this->log_request_as_not_cacheable(
+                                                    [
+                                                        'reason' => 'Request is AJAX',
+                                                        'extra'  => 'Constant DOING_AJAX defined and true',
                                                     ]
                                             );
             return false;
