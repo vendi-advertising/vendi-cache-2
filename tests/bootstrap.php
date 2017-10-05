ed906096c8d43a0e1fcf4172531cb8f5adbb07de
cjhaas <chris@vendiadvertising.com>
2017-10-05 16:17:17 -0500

    Cleanup bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,10 +8,4 @@ require_once $_tests_dir . '/includes/functions.php';
 // Start up the WP testing environment.
 require_once $_tests_dir . '/includes/bootstrap.php';
 
-// tests_add_filter(
-//                     'muplugins_loaded',
-//                     function()
-//                     {
-                        require dirname( dirname( __FILE__ ) ) . '/vendi-cache.php';
-                    // }
-                // );
+require dirname( dirname( __FILE__ ) ) . '/vendi-cache.php';
