800951e454f64b66ef8b03e22db5737c6e746483
Chris Haas <chris@vendiadvertising.com>
2017-10-06 05:52:52 -0500

    Made constant and function bypass tests generic

--- tests/CacheBypasses/test-AjaxMode.php
+++ tests/CacheBypasses/test-AjaxMode.php
@@ -10,18 +10,17 @@ class test_CacheBypasses_AjaxMode extends cache_bypass_base
     /**
      * @covers Vendi\Cache\CacheBypasses\AjaxMode::is_cacheable
      */
+    public function test_is_cacheable__DOING_AJAX__not_defined()
+    {
+        $this->_test_constant_not_defined( 'DOING_AJAX' );
+    }
+
+    /**
+     * @covers Vendi\Cache\CacheBypasses\AjaxMode::is_cacheable
+     */
     public function test_is_cacheable__DOING_AJAX__true()
     {
-        $maestro = $this->__get_maestro();
-        $cache_settings = $maestro->get_cache_settings();
-        $cache_settings->reset_all();
-
-        $this->assertFalse( $cache_settings->is_constant_defined( 'DOING_AJAX' ) );
-        $cache_settings->set_constant( 'DOING_AJAX', true );
-        $test = new AjaxMode( $maestro );
-        $result = $test->is_cacheable();
-        $this->assertFalse( $result );
-        $cache_settings->reset_all();
+        $this->_test_constant_defined_set_to_boolean( 'DOING_AJAX', false, true );
     }
 
     /**
@@ -29,15 +28,15 @@ class test_CacheBypasses_AjaxMode extends cache_bypass_base
      */
     public function test_is_cacheable__DOING_AJAX__false()
     {
-        $maestro = $this->__get_maestro();
-        $cache_settings = $maestro->get_cache_settings();
+        $this->_test_constant_defined_set_to_boolean( 'DOING_AJAX', true, false );
+    }
 
-        $this->assertFalse( $cache_settings->is_constant_defined( 'DOING_AJAX' ) );
-        $cache_settings->set_constant( 'DOING_AJAX', false );
-        $test = new AjaxMode( $maestro );
-        $result = $test->is_cacheable();
-        $this->assertTrue( $result );
-        $cache_settings->reset_all();
+    /**
+     * @covers Vendi\Cache\CacheBypasses\AjaxMode::is_cacheable
+     */
+    public function test_is_cacheable__wp_doing_ajax__not_defined()
+    {
+        $this->_test_function_not_defined( 'wp_doing_ajax' );
     }
 
     /**
@@ -45,15 +44,7 @@ class test_CacheBypasses_AjaxMode extends cache_bypass_base
      */
     public function test_is_cacheable__wp_doing_ajax__false()
     {
-        $maestro = $this->__get_maestro();
-        $cache_settings = $maestro->get_cache_settings();
-
-        $this->assertFalse( $cache_settings->is_function_defined( 'wp_doing_ajax' ) );
-        $cache_settings->set_function( 'wp_doing_ajax', function( ){ return false; } );
-        $test = new AjaxMode( $maestro );
-        $result = $test->is_cacheable();
-        $this->assertTrue( $result );
-        $cache_settings->reset_all();
+        $this->_test_function_defined_returns_boolean( 'wp_doing_ajax', true, false );
     }
 
     /**
@@ -61,16 +52,7 @@ class test_CacheBypasses_AjaxMode extends cache_bypass_base
      */
     public function test_is_cacheable__wp_doing_ajax__true()
     {
-        $maestro = $this->__get_maestro();
-        $cache_settings = $maestro->get_cache_settings();
-
-        $this->assertFalse( $cache_settings->is_function_defined( 'wp_doing_ajax' ) );
-        $cache_settings->set_function( 'wp_doing_ajax', function( ){ return true; } );
-        $test = new AjaxMode( $maestro );
-        $result = $test->is_cacheable();
-        $this->assertFalse( $result );
-        $cache_settings->reset_all();
+        $this->_test_function_defined_returns_boolean( 'wp_doing_ajax', false, true );
     }
-
 
 }
--- tests/includes/cache_bypass_base.php
+++ tests/includes/cache_bypass_base.php
@@ -21,4 +21,55 @@ class cache_bypass_base extends \PHPUnit_Framework_TestCase
                  )
             ;
     }
+
+    public function _test_constant_not_defined( $name )
+    {
+        $maestro = $this->__get_maestro();
+        $cache_settings = $maestro->get_cache_settings();
+
+        $this->assertFalse( $cache_settings->is_constant_defined( $name ) );
+        $test = new AjaxMode( $maestro );
+        $result = $test->is_cacheable();
+        $this->assertTrue( $result );
+        $cache_settings->reset_all();
+    }
+
+    public function _test_constant_defined_set_to_boolean( $name, $value, $is_cacheable )
+    {
+        $maestro = $this->__get_maestro();
+        $cache_settings = $maestro->get_cache_settings();
+        $cache_settings->reset_all();
+
+        $this->assertFalse( $cache_settings->is_constant_defined( $name ) );
+        $cache_settings->set_constant( $name, $value );
+        $test = new AjaxMode( $maestro );
+        $result = $test->is_cacheable();
+        $this->assertSame( $is_cacheable, $result );
+        $cache_settings->reset_all();
+    }
+
+    public function _test_function_not_defined( $name )
+    {
+        $maestro = $this->__get_maestro();
+        $cache_settings = $maestro->get_cache_settings();
+
+        $this->assertFalse( $cache_settings->is_function_defined( $name ) );
+        $test = new AjaxMode( $maestro );
+        $result = $test->is_cacheable();
+        $this->assertTrue( $result );
+        $cache_settings->reset_all();
+    }
+
+    public function _test_function_defined_returns_boolean( $name, $value, $is_cacheable )
+    {
+        $maestro = $this->__get_maestro();
+        $cache_settings = $maestro->get_cache_settings();
+
+        $this->assertFalse( $cache_settings->is_function_defined( $name ) );
+        $cache_settings->set_function( $name, function( ) use ( $value ) { return $value; } );
+        $test = new AjaxMode( $maestro );
+        $result = $test->is_cacheable();
+        $this->assertSame( $is_cacheable, $result );
+        $cache_settings->reset_all();
+    }
 }
