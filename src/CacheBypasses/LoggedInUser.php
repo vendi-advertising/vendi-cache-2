9df09a2f6dd8e2f400b1e15c9258bff3dbed8b7f
cjhaas <chris@vendiadvertising.com>
2017-10-19 16:54:45 -0500

    Add a better comment about get_current_user_id

--- src/CacheBypasses/LoggedInUser.php
+++ src/CacheBypasses/LoggedInUser.php
@@ -6,6 +6,10 @@ final class LoggedInUser extends AbstractCacheBypass
 {
     public function is_cacheable()
     {
+        //I have no way to test this. There's really no way for this method to
+        //get invoked without the function existing (since we load everything in
+        //plugins_loaded) but in the off-chance that we get invoked strangely,
+        //maybe through WP-CLI changes, I'm still including it.
         if (!function_exists('get_current_user_id')) {
             $this->log_request_as_not_cacheable(
                                                     [
