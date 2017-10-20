8cc8ceddc13eb92e2c206cd1ae895e20f9a3e9e3
cjhaas <chris@vendiadvertising.com>
2017-10-20 11:36:58 -0500

    Added comments to test base

--- tests/includes/vendi_cache_test_base.php
+++ tests/includes/vendi_cache_test_base.php
@@ -10,14 +10,17 @@ use Vendi\Cache\Tests\nullhandler_log_handler;
 
 class vendi_cache_test_base extends \WP_UnitTestCase
 {
+    //Array of directories that this test should cleanup when done
     private $_dirs = array();
 
+    //Array of files that this test should cleanup when done
     private $_files = array();
 
     public function tearDown()
     {
         parent::tearDown();
 
+        //Cleanup all files
         foreach( $this->_files as $f )
         {
             if( is_file( $f ) )
@@ -26,6 +29,9 @@ class vendi_cache_test_base extends \WP_UnitTestCase
             }
         }
 
+        //Cleanup all dirs
+        //NOTE: rmdir() will fail if the dir is not empty so remember to add
+        //each file you create to $this->_files, too!
         foreach( $this->_dirs as $d )
         {
             if( is_dir( $d ) )
@@ -35,38 +41,67 @@ class vendi_cache_test_base extends \WP_UnitTestCase
         }
     }
 
+    /**
+     * Create (or just touch) the supplied file and mark
+     * it for cleanup afterwards.
+     *
+     * @param string $path The absolute path to the file.
+     */
     public function touch_file( $path )
     {
         touch( $path );
         $this->_files[] = $path;
     }
 
-    //https://stackoverflow.com/a/1707859/231316
+    /**
+     * Create a unique temporary directory and mark it for cleanup afterwards.
+     *
+     * @see  https://stackoverflow.com/a/1707859/231316
+     * @return [type] [description]
+     */
     public function create_temp_dir()
     {
+
+        //tempnam actually creates a file, so do that first
         $tempfile = tempnam( sys_get_temp_dir(), 'VC2' );
         if( false === $tempfile )
         {
             throw new \Exception( 'Could not create file for temporary directory' );
         }
 
+        //Race-condition sanity check
         if( file_exists( $tempfile ) )
         {
             unlink( $tempfile );
         }
 
+        //Actually create the directory
         mkdir( $tempfile );
 
+        //Race-condition sanity double-check
         if( ! is_dir( $tempfile ) )
         {
             throw new \Exception( 'Could not create temporary directory' );
         }
 
+        //Mark the directory for cleanup
         $this->_dirs[] = $tempfile;
 
+        //Return the ABS path to the directory
         return $tempfile;
     }
 
+    /**
+     * Create a new maestro for all tests with optional parameters.
+     * @param  Request|null  $request         A specific Symfony Request or null
+     *                                        for the default Request.
+     * @param  callable|null $handle_function The function to invoke for each
+     *                                        log call or null for none.
+     * @param  string|null   $dir             The directory to bind the file
+     *                                        system adapter to or null for the
+     *                                        default.
+     * @return Maestro
+     */
     public function __get_new_maestro( Request $request = null, callable $handle_function = null, $dir = null )
     {
         $adapter = null;
