25f023cfc3a9a02cbea2902c2e0461596e446656
Chris Haas <chris@vendiadvertising.com>
2017-10-29 17:50:57 -0500

    Shortcircuit cookies for humbug testing

--- src/CacheBypasses/WpCookies.php
+++ src/CacheBypasses/WpCookies.php
@@ -7,30 +7,32 @@ final class WpCookies extends AbstractCacheBypass
     {
         $client_cookies = $this->get_cookies();
 
-        //wordpress_logged_in_[hash] cookies indicates logged in
-        if (count($client_cookies) > 0) {
-            $cookies_to_test = [
-                                    'comment_author',
-                                    'wp-postpass',
-                                    'wf_logout',
-                                    'wordpress_logged_in',
-                                    'wptouch_switch_toggle',
-                                    'wpmp_switcher',
-                            ];
+        //Return early for Humbug mutant testing.
+        if (0===count($client_cookies)) {
+            return false;
+        }
 
-            foreach ($client_cookies as $client_cookie => $value) {
-                foreach ($cookies_to_test as $cookie_to_test) {
-                    //contains a cookie which indicates user must not be cached
-                    if (mb_strpos($client_cookie, $cookie_to_test) !== false) {
-                        $this->log_request_as_not_cacheable(
-                                                                [
-                                                                    'reason' => 'Found special cookie',
-                                                                    'cookie' => $cookie_to_test,
-                                                                    'value'  => $value,
-                                                                ]
-                                                        );
-                        return true;
-                    }
+        $cookies_to_test = [
+                                'comment_author',
+                                'wp-postpass',
+                                'wf_logout',
+                                'wordpress_logged_in',
+                                'wptouch_switch_toggle',
+                                'wpmp_switcher',
+                        ];
+
+        foreach ($client_cookies as $client_cookie => $value) {
+            foreach ($cookies_to_test as $cookie_to_test) {
+                //contains a cookie which indicates user must not be cached
+                if (mb_strpos($client_cookie, $cookie_to_test) !== false) {
+                    $this->log_request_as_not_cacheable(
+                                                            [
+                                                                'reason' => 'Found special cookie',
+                                                                'cookie' => $cookie_to_test,
+                                                                'value'  => $value,
+                                                            ]
+                                                    );
+                    return true;
                 }
             }
         }
