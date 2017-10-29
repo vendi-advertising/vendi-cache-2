13d66c2b72e4fb93fee57b41db1e27cecae47b16
Chris Haas <chris@vendiadvertising.com>
2017-10-29 17:50:30 -0500

    For custom bypass, use a special log handler function with assertions

--- tests/includes/cache_bypass_base.php
+++ tests/includes/cache_bypass_base.php
@@ -3,9 +3,42 @@ namespace Vendi\Cache\Tests;
 
 class cache_bypass_base extends vendi_cache_test_base
 {
+    public $_logs = [];
+
+    public function assertSameLastMessage($expected, $do_not_purge_logs = false)
+    {
+        if (0===count($this->_logs)) {
+            throw new \Exception('No last message received');
+        }
+
+        $last_message = end($this->_logs);
+
+        if (!$do_not_purge_logs) {
+            $this->_purge_logs();
+        }
+
+        $this->assertArrayHasKey('message', $last_message);
+
+        $this->assertSame($expected, $last_message['message']);
+    }
+
+    public function _purge_logs()
+    {
+        $this->_logs = [];
+    }
+
+    public function _handle_logger(array $record)
+    {
+        $this->_logs[] = $record;
+    }
+
     public function _test_is_cacheable_because_fatal_constant_not_defined($class_to_test, $name)
     {
-        $maestro = $this->__get_new_maestro();
+        $logger = [];
+        $maestro = $this->__get_new_maestro(
+                                                null,
+                                                [$this, '_handle_logger']
+                                        );
         $cache_settings = $maestro->get_secretary();
 
         //The supplied constant should not exist by default
@@ -16,7 +49,6 @@ class cache_bypass_base extends vendi_cache_test_base
         $test = new $obj($maestro);
         $this->assertFalse($test->is_resource_not_cacheable_because_constant_is_true());
         $this->assertFalse($test->is_resource_not_cacheable());
-
         $cache_settings->set_constant($name, true);
 
         $obj = "\\Vendi\\Cache\\CacheBypasses\\$class_to_test";
@@ -24,11 +56,14 @@ class cache_bypass_base extends vendi_cache_test_base
         $this->assertTrue($test->is_resource_not_cacheable_because_constant_is_true());
         $this->assertTrue($test->is_resource_not_cacheable());
 
+        $this->assertSameLastMessage('Request not cacheable');
+
         //Constant is define but weirdly set to false.
         $cache_settings->set_constant($name, false);
 
         $obj = "\\Vendi\\Cache\\CacheBypasses\\$class_to_test";
         $test = new $obj($maestro);
         $this->assertFalse($test->is_resource_not_cacheable_because_constant_is_true());
+        $this->assertSameLastMessage('Strange state - constant set to false');
     }
 }
