ca6e34524d5e75969fb90d8c190fde7e4ae46339
cjhaas <chris@vendiadvertising.com>
2017-11-07 13:22:18 -0600

    Test for PSR-7 Request maker

---
+++ tests/test-VendiPsr7RequestMaker.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+namespace Vendi\Cache\Tests;
+
+use Vendi\Cache\VendiPsr7RequestMaker;
+
+class test_VendiPsr7RequestMaker extends vendi_cache_test_base
+{
+    /**
+     * @covers \Vendi\Cache\VendiPsr7RequestMaker::create_default_request
+     */
+    public function test_create_default_request()
+    {
+        $this->assertInstanceOf('Psr\Http\Message\ServerRequestInterface', VendiPsr7RequestMaker::create_default_request());
+    }
+}
