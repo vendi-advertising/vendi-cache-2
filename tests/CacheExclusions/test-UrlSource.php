6a63c784f8b231b77d6517f1ca6350666a330077
cjhaas <chris@vendiadvertising.com>
2017-11-09 13:55:34 -0600

    Tests for UrlSource

--- tests/CacheExclusions/test-UrlSource.php
+++ tests/CacheExclusions/test-UrlSource.php
@@ -4,62 +4,68 @@ namespace Vendi\Cache\Tests;
 use Vendi\Cache\CacheExclusions\Comparators\MatchesExactly;
 use Vendi\Cache\CacheExclusions\Sources\UrlSource;
 
+/**
+ * @group CacheExclusionUrlSource
+ */
 class test_all_sources extends vendi_cache_test_base
 {
-    /**
-     * @covers \Vendi\Cache\CacheExclusions\Sources\UrlSource::__construct
-     * @covers \Vendi\Cache\CacheExclusions\Sources\UrlSource::get_maestro
-     */
-    public function test_url()
-    {
-        $request = $this->__create_server_request_from_url('http://www.example.net/cheese');
-
-        $url_source = new UrlSource($this->__get_new_maestro($request));
-        $this->assertInstanceOf('Vendi\Cache\Maestro', $url_source->get_maestro());
-        $this->assertTrue($url_source->should_request_be_excluded_from_caching(new MatchesExactly(), '/cheese'));
-
-        // $possible_sources = [
-        //                         'url'               => 'UrlSource',
-        //                         'user-agent'        => 'UserAgentSource',
-        //                         'cookie-name'       => 'CookieNameSource',
-        // ];
-
-        // $possible_comparators = [
-        //                         'matches-exactly'   => 'MatchesExactly',
-        //                         'contains'          => 'Contains',
-        //                         'ends-with'         => 'EndsWith',
-        //                         'starts-with'       => 'StartsWith',
-        // ];
-
-        // $root_namespace       = 'Vendi\Cache';
-        // $exclusion_namespace  = "$root_namespace\\CacheExclusions";
-        // $source_namespace     = "$exclusion_namespace\\Sources";
-        // $comparator_namespace = "$exclusion_namespace\\Comparators";
-
-        // foreach ($possible_sources as $source_key => $souce_class_name) {
-        //     $class_name = "$source_namespace\\$souce_class_name";
-
-        //     $obj = new $class_name($this->__get_new_maestro());
-
-        //     $this->assertSame($source_key, $obj->get_storage_name());
-        //     $this->assertSame('cheese', $obj->get_value());
-        // }
-    }
-
-    /**
-     * @covers \Vendi\Cache\CacheExclusions\Sources\UrlSource::get_url_path
-     */
-    public function test__get_url_path()
-    {
-        $request = $this->__create_server_request_from_url('http://www.example.net/cheese');
-        $this->assertSame('/cheese', (new UrlSource($this->__get_new_maestro($request)))->get_url_path());
-    }
-
     /**
      * @covers \Vendi\Cache\CacheExclusions\Sources\UrlSource::get_storage_name
      */
     public function test__get_storage_name()
     {
         $this->assertSame('url', (new UrlSource($this->__get_new_maestro()))->get_storage_name());
+    }
+
+    /**
+     * @covers \Vendi\Cache\CacheExclusions\Sources\UrlSource::should_request_be_excluded_from_caching
+     * @covers \Vendi\Cache\CacheExclusions\Sources\UrlSource::get_url_path
+     * @covers \Vendi\Cache\CacheExclusions\Sources\UrlSource::__construct
+     * @covers \Vendi\Cache\CacheExclusions\Sources\UrlSource::get_maestro
+     * @dataProvider provider_for__test__should_request_be_excluded_from_caching
+     */
+    public function test__should_request_be_excluded_from_caching($expected, $class_name_base, $url, $string_to_test)
+    {
+        $root_namespace       = 'Vendi\Cache';
+        $exclusion_namespace  = "$root_namespace\\CacheExclusions";
+        $comparator_namespace = "$exclusion_namespace\\Comparators";
+        $class_name_full      = "$comparator_namespace\\$class_name_base";
+
+        $request = $this->__create_server_request_from_url($url);
+        $source = new UrlSource($this->__get_new_maestro($request));
+
+        $this->assertInstanceOf('Vendi\Cache\Maestro', $source->get_maestro());
+
+        $comparator = new $class_name_full();
+        $this->assertSame($expected, $source->should_request_be_excluded_from_caching($comparator, $string_to_test));
+    }
+
+    public function provider_for__test__should_request_be_excluded_from_caching()
+    {
+        return [
+                    [ true,  'MatchesExactly', 'http://www.example.net/cheese', '/cheese' ],
+                    [ false, 'MatchesExactly', 'http://www.example.net/chees',  '/cheese' ],
+                    [ false, 'MatchesExactly', 'http://www.example.net/cheese', '/chees' ],
+
+                    [ true,  'Contains',       'http://www.example.net/cheese', '/cheese' ],
+                    [ true,  'Contains',       'http://www.example.net/cheese', 'che' ],
+                    [ true,  'Contains',       'http://www.example.net/cheese', '/che' ],
+                    [ false, 'Contains',       'http://www.example.net/cheese', 'chs' ],
+
+                    [ true,  'StartsWith',     'http://www.example.net/cheese', '/cheese' ],
+                    [ true,  'StartsWith',     'http://www.example.net/cheese', '/chee' ],
+                    [ true,  'StartsWith',     'http://www.example.net/cheese', '/' ],
+                    [ false,  'StartsWith',     'http://www.example.net/cheese', 'cheese' ],
+
+                    [ true,  'EndsWith',       'http://www.example.net/cheese', '/cheese' ],
+                    [ true,  'EndsWith',       'http://www.example.net/cheese', 'ese' ],
+                    [ false, 'EndsWith',       'http://www.example.net/cheese', '/chees' ],
+
+                    //PSR-7 allows the path to not start with a slash however we do. These two
+                    //are considered equal from the caching perspective
+                    [ true,  'MatchesExactly', 'http://www.example.net/', '/' ],
+                    [ true,  'MatchesExactly', 'http://www.example.net', '/' ],
+
+        ];
     }
 }
